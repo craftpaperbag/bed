eab1888c5f46d01c8256efe72c457a1964c1743b
craftpaperbag <kazu11518@gmail.com>
2015-02-07 21:30:18 +0900

    save

--- bed.js
+++ bed.js
@@ -1,62 +1,24 @@
 var makeCircleChartData = function (list) {
-  /* [
-   *   {
-   *    value: 100,
-   *    color: "#aaa",
-   *    highlight: "#ccc",
-   *    label: "My Label",
-   * } ] 
-   */
   var data = [
     {
-      value: 7*12,
-      color: "#aaa",
-      highlight: "#ccc",
+      value: 5*60,
+      color: "#2a2c2b",
+      highlight: "#2a2c2b",
       label: "sleeping"
     },
-  
     {
-      value: 12*12,
-      color: "#13df33",
-      highlight: "#55ff88",
+      value: 7*60,
+      color: "#ee4458",
+      highlight: "#ee4458",
       label: "awakening"
     },
     {
-      value: 5*12,
-      color: "#eee",
-      highlight: "#fff",
+      value: 12*60,
+      color: "#e2e6f3",
+      highlight: "#e2e6f3",
       label: "no data"
     },
   ]
-  return data;
-}
-
-var make24hChartData = function (list /* [ { label: "abc", data: [1,2,3...] } ...] */) {
-  var h24 = [];
-  for (var i=0; i<24; i++) {
-    h24.push(i);
-  }
-  var datasets = [];
-  for (var i in list) {
-    var e = list[i];
-    var dataset = {
-      label: e["label"],
-      data: e["data"],
-      fillColor: "rgba(220,220,220,0.2)",
-      strokeColor: "rgba(220,220,220,1)",
-      pointColor: "rgba(220,220,220,1)",
-      pointStrokeColor: "#fff",
-      pointHighlightFill: "#fff",
-      pointHighlightStroke: "rgba(220,220,220,1)",
-      // and more options...
-    };
-    datasets.push(dataset);
-  }
-  var data = {
-    labels: h24,
-    datasets: datasets,
-  };
-  console.log(data)
   return data;
 }
 
@@ -77,22 +39,26 @@ if (Meteor.isServer) {
 if (Meteor.isClient) {
   Meteor.startup(function () {
     // setup graph
-    var ctx = document.getElementById("graph").getContext("2d");
-    var data = make24hChartData([
-      {label: "hoge", data: [1,2,3]}  
-    ]);
     Chart.defaults.global.responsive = true;
-    var graph = new Chart(ctx).Line(data);
-
     var circleCtx = document.getElementById("circle").getContext("2d");
     var circleData = makeCircleChartData();
-    var circleGraph = new Chart(circleCtx).Pie(circleData)
-    var circleCtx = document.getElementById("circle2").getContext("2d");
-    var circleData = makeCircleChartData();
-    var circleGraph = new Chart(circleCtx).Pie(circleData)
-    var circleCtx = document.getElementById("circle3").getContext("2d");
-    var circleData = makeCircleChartData();
-    var circleGraph = new Chart(circleCtx).Pie(circleData)
+    var options = {
+      segmentShowStroke: false,
+    };
+    var circleGraph = new Chart(circleCtx).Pie(circleData, options);
+  });
+
+  Template.body.helpers({
+    hours: function () {
+      var hours = [];
+      for (var i=0; i<24; i++) {
+        hours.push({
+          hour: i,
+          sleepRatio: 1.0, /* 1: 全部寝てた 1>x>0:寝てた比率 0: 起きてた null: データなし */
+        });
+      }
+      return hours;
+    },
   });
 
 /*
