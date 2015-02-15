0d49197794c2ff892291ae1ca9216907280a4d29
craftpaperbag <kazu11518@gmail.com>
2015-02-15 20:33:06 +0900

    #5 + client/router.js

--- client/bed.js
+++ client/bed.js
@@ -4,28 +4,6 @@
 console.log("bed.js load start")
 
 //
-// Router
-//
-
-Router.configure({
-  layoutTemplate: 'layout',
-  notFoundTemplate: 'notFound'
-});
-
-var HomeController = RouteController.extend({
-  template: 'home'
-});
-
-Router.map(function() {
-
-  this.route('home', {
-    path: '/',
-    controller: HomeController
-  });
-
-});
-
-//
 // Subscribe
 //
 
